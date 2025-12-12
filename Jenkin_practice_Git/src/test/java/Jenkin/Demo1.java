ef25032b4a42b144bbe7219da00597be9cce50dd
ashamit <ashwiniamit13@gmail.com>
2025-12-12 18:11:06 +0530

    first commit

---
+++ Jenkin_practice_Git/src/test/java/Jenkin/Demo1.java
@@ -0,0 +1,10 @@
+package Jenkin;
+
+public class Demo1 
+{
+	public void m1()
+	{
+		System.out.println("Git through execution using Jenkin");
+	}
+
+}
---
+++ Jenkin_practice_Git/src/test/java/Jenkin/Demo2.java
@@ -0,0 +1,10 @@
+package Jenkin;
+
+public class Demo2 
+{
+	public void m2()
+	{
+		System.out.println("Demo of another class");
+	}
+
+}
