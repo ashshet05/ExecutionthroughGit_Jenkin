a9912cdd470b1bd7ce4cd187c8b2f5cb7f8f1c14
ashamit <ashwiniamit13@gmail.com>
2025-12-13 12:04:01 +0530

    Listener updates

--- Jenkin_practice_Git/src/main/java/com/qa/ExtentReportListener/ExtentReportListener.java
+++ Jenkin_practice_Git/src/main/java/com/qa/ExtentReportListener/ExtentReportListener.java
@@ -1,86 +1,41 @@
 package com.qa.ExtentReportListener;
 
+import org.testng.ITestContext;
+import org.testng.ITestResult;
+
+
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import org.testng.ITestListener;
-import org.testng.ITestResult;
-import org.testng.ITestContext;
 
-public class ExtentReportListener  implements ITestListener
-{
+import Reports.ExtentManager;
 
-		//WebDriver driver;
-		ExtentReports report;
-		ExtentTest test;
-		public void onTestStart(ITestResult result) {
-			
 
-			/* Step:3 Create a test method during the test execution start*/
-		test=report.createTest(result.getMethod().getMethodName());
-		}
+public class ExtentReportListener implements ITestListener{
+	 private static ExtentReports extent = ExtentManager.getInstance();
+	    private static ThreadLocal<ExtentTest> test = new ThreadLocal<>();
 
-		public void onTestSuccess(ITestResult result) {
-			 
-			 /*Step:4 Log the pass method*/
-			test.log(Status.PASS, result.getMethod().getMethodName()+" is passed");
-				
-			
-		}
+	    @Override
+	    public void onTestStart(ITestResult result) {
+	        ExtentTest extentTest =
+	                extent.createTest(result.getMethod().getMethodName());
+	        test.set(extentTest);
+	    }
 
-		public void onTestSkipped(ITestResult result) {
-			
-	/*step:6 Log the skip method()*/
-			
-			test.log(Status.SKIP, result.getMethod().getMethodName());
-			test.log(Status.SKIP, result.getThrowable());
-			
-		}
-	  public void onStart(ITestContext context) {
-			
-	/*Step:1 Extent report configuration*/
-			
-			Date d = new Date();
-			System.out.println(d);
-			String date = d.toString().replace(" ", "-").replace(":", "-");
-			
-			ExtentSparkReporter htmlReport=new ExtentSparkReporter(new File("extentreport.html"));				
-	        htmlReport.config().setDocumentTitle("Extent Report");
-	        htmlReport.config().setTheme(Theme.STANDARD);
-	        htmlReport.config().setReportName("Functional Test");
+	    public void onTestSuccess(ITestResult result) {
+	        test.get().pass("Test Passed");
+	    }
 
-			/*Step:2 Attach the physical report and do the system configuration*/
-			
-			report = new ExtentReports();
-			report.attachReporter(htmlReport);
-			report.setSystemInfo("OS", "Windows 11");
-			report.setSystemInfo("Environment", "Testing Environment");
-			report.setSystemInfo("URL", "http://localhost:8888");
-			report.setSystemInfo("Reporter Name", "Shobha");
-			}
+	    public void onTestFailure(ITestResult result) {
+	        test.get().fail(result.getThrowable());
+	    }
 
-			public void onTestFailure(ITestResult result) {
-			String testData = result.getMethod().getMethodName();
-			
-			 System.out.println("------execute-----");
-				
-			  EventFiringWebDriver edriver = new EventFiringWebDriver(BaseClass.sdriver);
-			  File src= edriver.getScreenshotAs(OutputType.FILE);
-			 
-			  try
-			  {
-				 // FileUtils.copyFile(src,new File("./ScreenShot/"+testName+".png"));
-				  FileUtils.copyFile(src,new File("./ScreenShot/"+testData+".png")); 
-			  }
-			  catch (Exception e) {
-			e.printStackTrace();
-			}
-			
-			
-		}
-	public void onFinish(ITestContext context) {
-		
-		report.flush();
-			
-}
-		
-}
+	    @Override
+	    public void onTestSkipped(ITestResult result) {
+	        test.get().skip("Test Skipped");
+	    }
+
+	    @Override
+	    public void onFinish(ITestContext context) {
+	        extent.flush();
+	    }}
