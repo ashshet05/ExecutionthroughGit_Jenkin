3b51c908146132b2216b063a2be0eefabb11074d
ashamit <ashwiniamit13@gmail.com>
2025-12-15 11:37:43 +0530

    trying in again

--- Jenkin_practice_Git/src/test/java/Jenkin/Demo1.java
+++ Jenkin_practice_Git/src/test/java/Jenkin/Demo1.java
@@ -8,6 +8,8 @@ public class Demo1
 	public void m1()
 	{
 		System.out.println("Git through execution using Jenkin");
+		System.out.println("Jenknin trigger Git as and when new build pushed to Git");
+		//Hello again i am checking
 	}
 
 }
