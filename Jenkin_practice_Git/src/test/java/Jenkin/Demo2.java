a849ae49678ab4b8a73ec15c8d6fdd2cd17e0e84
ashamit <ashwiniamit13@gmail.com>
2025-12-15 11:35:34 +0530

    one more check

--- Jenkin_practice_Git/src/test/java/Jenkin/Demo2.java
+++ Jenkin_practice_Git/src/test/java/Jenkin/Demo2.java
@@ -9,6 +9,7 @@ public class Demo2
 	{
 		System.out.println("Demo of another class");
 		System.out.println("Jenknin trigger Git as and when new build pushed to Git");
+		//Hello again i am checking
 	}
 
 }
